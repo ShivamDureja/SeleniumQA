d65d9862c1caa89a8c3a8bbe5fa2b32ad07b24f9
Shivam Dureja <shivamdureja13@gmail.com>
2022-07-14 15:45:47 +0530

    Update automate.py

--- automate.py
+++ automate.py
@@ -1,52 +1,45 @@
 from selenium.webdriver.common.by import By
 import time
+from bs4 import BeautifulSoup
+
 
 def automate(driver,dataList):
-    questionList = getQuestionDivsFromWeb(driver)
-    first = "Given three equal resistors, how many different combinations (taken all of them together) can be made?"
-    for questionDiv in questionList:
-        print("LOADING QUESTION")
-        questions = questionDiv.find_elements(by=By.TAG_NAME, value="span")
-        # selectOption(questionDiv, "3")
-        for q in questions:
-            if(q.text == first):
-                print(q.tag_name)
-        # questionSpan = questions[3]
-        # print(questions.tag_name)
-        # questionElement = question.find_element(by=By.TAG_NAME, value="span")
-        # questionText = questionElement.text
-        # print(questionText)
-        # found = 0
-        # questionData = []
-        # for list in dataList:
-        #     print("Working for question : " + questionText)
-        #     if list[0] == questionText:
-        #         found = 1
-        #         questionData = list
-        # if found:
-        #     selectOption(questionElement, questionData[5])
-        #     time.sleep(1)
-        
-def traverse(path,ques):
-    for i in range(ques):
+    time.sleep(2)
+    nav = driver.find_element(by=By.CLASS_NAME, value = "Navigation-sc-__sc-1ckh2u2-4")
+    btn = nav.find_elements(by=By.TAG_NAME, value = "button")
+    fSets = getQuestionDivsFromWeb(driver)
+    i = 0
+    for fset in fSets:
+        ques = None
+        selectedFSet = None
+        currQ = None
+        soup = BeautifulSoup(fset.get_attribute('innerHTML'),"html.parser")
+        i = i + 1
+        for list in dataList:
+            ques = soup.find(text = f'{list[0]}')
+            if(ques != None):
+                currQ = list
+                selectedFSet = fset
+        if(selectedFSet != None):
+            ans = soup.find(text = f'{currQ[5]}')
+            selectOption(selectedFSet,ans)
+            time.sleep(2)
+        else:
+            if(i == len(fSets)):
+                continue
+            traverse(btn[0],1)
+
+
+def traverse(path,clicks):
+    for i in range(clicks):
         time.sleep(1)
         path.click()
 
 def getQuestionDivsFromWeb(driver):
     fSets = driver.find_elements(by=By.TAG_NAME, value="fieldset")
-    # for sets in fSets:
-    #     print(sets.tag_name)
     return fSets
 
-
-def selectOption(parent, answer):
-    print("MOVING TO SELECT OPTION")
-    # pXpath = parent.find_element(by=By.XPATH, value="../../../../..")
-    # print("pXpath : " + pXpath.tag_name)
-    wrapper = parent.find_element(by=By.CLASS_NAME, value="fZIlJA")
-    # print(wrapper.tag_name)
-    option = wrapper.find_element(by=By.XPATH, value= f"//div[@aria-label='{answer}']")
-    time.sleep(5)
+def selectOption(fieldset,answer):
+    option = fieldset.find_element(by=By.XPATH, value=f".//div[@aria-label='{answer}']")
     option.click()
-    time.sleep(5)
-
+    time.sleep(2)
