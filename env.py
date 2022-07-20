e0df0961f56fd7bdf45248f436c14ea66bb68699
Shivam Dureja <shivamdureja13@gmail.com>
2022-07-20 15:01:52 +0530

    created env file to store all variables

---
+++ env.py
@@ -0,0 +1,26 @@
+###   VARIABLES   ###
+
+##### for main.py #####
+pageUrl = "https://tzflz53oiau.typeform.com/to/n7eaeDxl"
+qFile_path = "D:\\SMDEVOPS\\SeleniumQA\\updatedQues.xlsx"
+
+##### for automate.py #####
+QuesCount = 9
+# Value of Xpath of Wrapper element for every question present
+WrapperValue = "//*[@data-qa='question-wrapper']"
+# value for answer element of type FIB (Tag Name)
+FibValue = "textarea"
+# value for answer element of type FIB (Tag Name)
+LinkedValue = "input"
+
+##### for Loader.py #####
+# Enter the location of your chrome.exe
+chromePath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+# Enter the location of your chrome driver
+chromeDriver = "D:\SeleniumChromedriver\chromedriver.exe"
+
+##### for humanMimicking.py #####
+# time taken to move mouse (slower the time more is the speed of mouse)
+mouseTime = 0.5
+# additional vertical height to be added
+additionalHeight = 85
